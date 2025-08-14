36b17322ff58134ebc01788a95240ab9a099ba56
CowboyGH <kocergindenis81@gmail.com>
2025-08-14 10:55:32 +0700

    refactor: simplify main function and update entry point
    - Replaced the main function to directly call the runner
    - Replace the MainApp widget to 'features/app' directory
    - Streamlined the application startup process
    - Updated version to 0.1.4

---
+++ lib/runner.dart
@@ -0,0 +1,7 @@
+import 'package:currency_rates/features/app/app.dart';
+import 'package:flutter/material.dart';
+
+void run() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const CurrencyRatesApp());
+}
