0166f51106dc72475afbca6e142b6bdfa94a32d8
CowboyGH <kocergindenis81@gmail.com>
2025-08-14 14:26:59 +0700

    feat: add theming support with light and dark modes
    - Introduced `AppThemeData` for managing application themes
    - Added `AppColorTheme` for defining color schemes
    - Created light and dark color themes for better UI experience
    - Updated version to 0.1.6

---
+++ lib/uikit/themes/app_theme_data.dart
@@ -0,0 +1,46 @@
+import 'package:currency_rates/uikit/themes/colors/app_color_theme.dart';
+import 'package:flutter/material.dart';
+
+/// Тема приложения
+abstract class AppThemeData {
+  static const _lightColorTheme = AppColorTheme.light();
+  static const _darkColorTheme = AppColorTheme.dark();
+
+  /// Светлая тема
+  static final lightTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: _lightColorTheme.background,
+    colorScheme: ColorScheme.light(
+      primary: _lightColorTheme.primary,
+      onPrimary: _lightColorTheme.onPrimary,
+      secondary: _lightColorTheme.secondary,
+      onSecondary: _lightColorTheme.onSecondary,
+      error: _lightColorTheme.error,
+      surface: _lightColorTheme.surface,
+      onSurface: _lightColorTheme.onSurface,
+    ),
+    disabledColor: _lightColorTheme.disabled,
+    dividerColor: _lightColorTheme.divider,
+    splashFactory: NoSplash.splashFactory,
+    extensions: [_lightColorTheme],
+  );
+
+  /// Темная тема
+  static final darkTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: _darkColorTheme.background,
+    colorScheme: ColorScheme.dark(
+      primary: _darkColorTheme.primary,
+      onPrimary: _darkColorTheme.onPrimary,
+      secondary: _darkColorTheme.secondary,
+      onSecondary: _darkColorTheme.onSecondary,
+      error: _darkColorTheme.error,
+      surface: _darkColorTheme.surface,
+      onSurface: _darkColorTheme.onSurface,
+    ),
+    disabledColor: _darkColorTheme.disabled,
+    dividerColor: _darkColorTheme.divider,
+    splashFactory: NoSplash.splashFactory,
+    extensions: [_darkColorTheme],
+  );
+}
