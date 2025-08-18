7a33832e0dfd8a417a53c9b42115fed7be5d0d3b
CowboyGH <kocergindenis81@gmail.com>
2025-08-18 13:18:40 +0700

    feat: update async result typedef
    - Modified `AsyncResult` typedef to simplify its usage for asynchronous results
    - Updated version to 0.1.11

--- lib/core/domain/entities/result/async_result.dart
+++ lib/core/domain/entities/result/async_result.dart
@@ -1,3 +1,5 @@
 import 'package:currency_rates/core/domain/entities/result/result.dart';
 
-typedef AsyncResult<S extends Object, F extends Object> = Future<Result<S, F>>;
+/// Асинхронный [Result] для всех методов, которые могут упасть.
+/// В большинстве случаев это методы репозитория.
+typedef AsyncResult<T> = Future<Result<T, Failure>>;
