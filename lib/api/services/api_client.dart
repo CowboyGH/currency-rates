96774e8f188c35793e7e03b4ded6c349bc7dc24a
CowboyGH <kocergindenis81@gmail.com>
2025-08-18 15:30:04 +0700

    feat: add API client
    - Added `ApiClient` for handling API requests
    - Implemented `getCurrencies` method to fetch currency data
    - Created `ApiUrls` class for managing API endpoints
    - Integrated Dio for network requests
    - Registered `ApiClient` in DI
    - Updated version to 0.2.2

---
+++ lib/api/services/api_client.dart
@@ -0,0 +1,15 @@
+import 'package:currency_rates/api/services/api_urls.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_client.g.dart';
+
+/// REST-клиент._
+@RestApi()
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
+
+  /// Получение списка валют
+  @GET(ApiUrls.currencies)
+  Future<HttpResponse<String>> getCurrencies();
+}
--- lib/features/app/di/di.dart
+++ lib/features/app/di/di.dart
@@ -1,3 +1,5 @@
+import 'package:currency_rates/api/services/api_client.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 /// Глобальный экземпляр сервис-локатора GetIt для управления зависимостями.
@@ -5,4 +7,19 @@ final di = GetIt.instance;
 
 /// Инициализирует зависимости приложения с помощью GetIt.
 /// Включает регистрацию слоёв data, domain, presentation.
-void initDi() {}
+void initDi() {
+  // ApiClient
+  const sendTimeout = Duration(seconds: 10);
+  const connectTimeout = Duration(seconds: 10);
+  const receiveTimeout = Duration(seconds: 20);
+
+  final dio = Dio();
+  dio.options
+    ..baseUrl = 'http://www.cbr.ru/'
+    ..sendTimeout = sendTimeout
+    ..connectTimeout = connectTimeout
+    ..receiveTimeout = receiveTimeout;
+
+  final ApiClient apiClient = ApiClient(dio);
+  di.registerLazySingleton(() => apiClient);
+}
