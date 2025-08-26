f5f06b7a4eb90ea9e7fe071e1ca551f5094bb501
CowboyGH <kocergindenis81@gmail.com>
2025-08-26 13:46:35 +0700

    fix: clean up useless app string
    - Removed unused `retry` string constant from `AppStrings`
    - Updated app version to 0.2.38

--- lib/assets/strings/app_strings.dart
+++ lib/assets/strings/app_strings.dart
@@ -4,9 +4,6 @@ abstract class AppStrings {
   static const unknownError = 'Неизвестная ошибка';
   static const refreshSuccess = 'Курс валют актуальный, обновление не требуется';
 
-  // Кнопки
-  static const retry = 'Повторить';
-
   // Список валют
   static const currencyRatesOn = 'Курсы валют на';
 
