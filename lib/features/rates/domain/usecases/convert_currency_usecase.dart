7087a7212fa2d2946cc1199e77d1314ec72f6bfa
CowboyGH <kocergindenis81@gmail.com>
2025-08-27 15:05:47 +0700

    feat: add currency conversion use case
    - Introduced `ConvertCurrencyUsecase` for converting amounts from selected currencies to rubles
    - Implemented error handling for various scenarios: negative amounts, zero amounts, underflow, and overflow
    - Added unit tests for `ConvertCurrencyUsecase` to ensure correct functionality
    - Updated version to 0.3.2

---
+++ lib/features/rates/domain/usecases/convert_currency_usecase.dart
@@ -0,0 +1,40 @@
+import 'package:currency_rates/core/domain/entities/failure/app_failure.dart';
+import 'package:currency_rates/core/domain/entities/failure/conversion/conversion_failure.dart';
+import 'package:currency_rates/core/domain/entities/result/result.dart';
+import 'package:decimal/decimal.dart';
+
+/// Конвертирует сумму из выбранной валюты в рубли.
+/// Параметры:
+/// - [unitRate]: Курс валюты по отношению к рублю.
+/// - [amount]: Количество выбранной валюты.
+class ConvertCurrencyUsecase {
+  const ConvertCurrencyUsecase();
+
+  // Минимально допустимое положительное значение
+  static final Decimal minPositive = Decimal.parse(
+    '1e-28',
+  );
+
+  Result<Decimal, AppFailure> call({
+    required Decimal unitRate,
+    required Decimal amount,
+  }) {
+    try {
+      // Отрицательное значение
+      if (amount < Decimal.zero) return Result.failure(NegativeAmountFailure());
+      // Нулевое значение
+      if (amount == Decimal.zero) return Result.failure(ZeroAmountFailure());
+      // Слишком маленькое значение
+      if (amount < minPositive) return Result.failure(UnderflowFailure());
+      // Слишком большое значение
+      final digitsOnly = amount.toString().replaceAll(RegExp(r'[^0-9]'), '');
+      if (digitsOnly.length > 30) return Result.failure(OverflowFailure());
+
+      return Result.success(unitRate * amount);
+    } catch (e) {
+      return Result.failure(
+        UnknownConversionFailure(e is Exception ? e : Exception(e.toString())),
+      );
+    }
+  }
+}
