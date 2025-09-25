4ba6f5fe356757b93a451e366ac97182ffc9bfc4
CowboyGH <kocergindenis81@gmail.com>
2025-09-25 10:18:37 +0700

    feat: add HistoryCubit and HistoryState for managing history
    - Introduced `HistoryCubit` to manage the state of conversion history.
    - Added `HistoryState` class with various states for loading, success, and error scenarios.
    - Covered cubit with tests: success and failure cases
    - Updated version to 0.4.18

---
+++ lib/features/history/presentation/cubits/history/history_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:currency_rates/core/domain/entities/failure/app_failure.dart';
+import 'package:currency_rates/core/domain/entities/result/result.dart';
+import 'package:currency_rates/features/common/domain/entities/conversion_record_entity.dart';
+import 'package:currency_rates/features/history/domain/usecases/get_history_usecase.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'history_state.dart';
+
+/// Кубит для управления состоянием истории конвертаций.
+class HistoryCubit extends Cubit<HistoryState> {
+  final GetHistoryUsecase _getHistoryUsecase;
+
+  HistoryCubit(this._getHistoryUsecase) : super(HistoryInitial());
+
+  /// Загрузка истории конвертаций.
+  Future<void> loadHistory() async {
+    emit(HistoryLoading());
+    final result = await _getHistoryUsecase();
+    switch (result) {
+      case Success(:final data):
+        emit(HistoryLoadSuccess(data));
+      case Failure(:final error):
+        emit(HistoryLoadError(error));
+    }
+  }
+}
